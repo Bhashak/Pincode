4d1be1d7c8239c186957950670fcc10d77c9a61c
Bhashak <158458992+Bhashak@users.noreply.github.com>
2024-05-28 22:50:42 +0530

    Update pincode.java

--- src/test/java/Test/pincode.java
+++ src/test/java/Test/pincode.java
@@ -21,8 +21,8 @@ public class pincode {
 
     @Test
     public void pincode() throws InterruptedException, IOException {
-       WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
         driver.manage().window().maximize();
 
         FileInputStream excelFile = new FileInputStream(".//src//main//java//Resources//poincode.xlsx");
