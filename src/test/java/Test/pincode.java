7ae32d13940bd81abcfabd0f9109482ba8372c68
Bhashak <158458992+Bhashak@users.noreply.github.com>
2024-07-03 14:13:51 +0530

    Update pincode.java

--- src/test/java/Test/pincode.java
+++ src/test/java/Test/pincode.java
@@ -18,18 +18,11 @@ public  class pincode {
     public static String pincode = "";
     static WebDriver driver;
     @Test
-<<<<<<< HEAD
     public void Test1() throws InterruptedException, IOException {
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
-=======
-    public void pincode() throws InterruptedException, IOException {
-      WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
->>>>>>> c08e5d9f8804d532cb90b4c8a11b68e0e28400a3
+        WebDriverManager.edgedriver().setup();        driver = new EdgeDriver();
         driver.manage().window().maximize();
-        driver.get("https:///www.google.com");
-        FileInputStream excelFile = new FileInputStream("C:\\DQG\\DQG_WEB\\src\\main\\java\\Resources\\Book1k.xlsx");
+
+        FileInputStream excelFile = new FileInputStream("C:\\DQG\\DQG_WEB\\src\\main\\java\\Resources\\Book7500.xlsx");
         Workbook workbook = new XSSFWorkbook(excelFile);
         Sheet sheet = workbook.getSheetAt(0);
 
@@ -40,36 +33,24 @@ public  class pincode {
             String state = row.getCell(2).getStringCellValue();
             String city = row.getCell(3).getStringCellValue();
 
-            driver.get("https://www.google.com");
+            driver.get("https://www.bing.com/");
 
-//            WebElement search=  driver.findElement(By.xpath("//textarea[@name='q']"));
-//            WebElement search=driver.findElement(By.cssSelector("#APjFqb"));
-//            if(!search.isDisplayed())
-//            {
-//                refreshBrowser(driver);
-//            }
-            try {
-                WebElement search=driver.findElement(By.cssSelector("#APjFqb"));
-                search.sendKeys(    continent+ "," +country+ "," +state + "," + city + " pincode", Keys.ENTER);
-
-            }catch (NoSuchElementException noe)
+            WebElement search=  driver.findElement(By.xpath("//textarea[@name='q']"));
+            if(!search.isDisplayed())
             {
-                driver.navigate().refresh();
+                refreshBrowser(driver);
             }
+            search.sendKeys(    continent+ "," +country+ "," +state + "," + city + " Common pincode", Keys.ENTER);
 
 
             try {
-//                WebElement pincodeElement = driver.findElement(By.xpath("//div[@class='b_focusTextLarge' or @class='b_focusTextMedium']"));
-                WebElement pincodeElement = driver.findElement(By.xpath("//div[@class='bVj5Zb FozYP'][1]"));
-
+                WebElement pincodeElement = driver.findElement(By.xpath("//div[@class='b_focusTextLarge' or @class='b_focusTextMedium']"));
                 pincode = pincodeElement.getText();//div[@class='b_focusTextLarge' or @class='b_focusTextMedium']
                 System.out.println(pincode);
             } catch (NoSuchElementException e) {
                 System.out.println("First element not found at Row Number: " + rowNum + ", trying next element.");
                 try {
-//                    WebElement element1 = driver.findElement(By.xpath("//p[@class='b_lineclamp3 b_algoSlug']//strong[1]"));
-                    WebElement element1 = driver.findElement(By.xpath("//div[@class='HwtpBd gsrt PZPZlf kTOYnf']//div[@class='Z0LcW t2b5Cf']"));
-
+                    WebElement element1 = driver.findElement(By.xpath("//p[@class='b_lineclamp3 b_algoSlug']//strong[1]"));
                     if (element1.isDisplayed()) {
 
                         try {
@@ -84,42 +65,20 @@ public  class pincode {
                 } catch (NoSuchElementException e1) {
                     System.out.println("Second element not found at Row Number: " + rowNum + ", trying next element.");
                     try {
-                        WebElement elemzip = driver.findElement(By.xpath("//a[@class='FLP8od']"));
-                        pincode=elemzip.getText();
-                        System.out.println(pincode);
-
-                    } catch (NoSuchElementException p1) {
-                        System.out.println("Not Found");
-                        pincode="Not Found";
-                        try{
-                            WebElement ee=driver.findElement(By.xpath("//span//em[1]"));
-                            if (ee.isDisplayed()) {
-
-                                try {
-                                    pincode = ee.getText();
-                                    int number = Integer.parseInt(pincode);
-                                    // Output: 123
-                                } catch (NumberFormatException e11) {
-                                    pincode="Not Found";
-                                }
-                                System.out.println(pincode);
-                            }
-                        }catch (NoSuchElementException ee0) {
-                            pincode = "Not Found";
-                            try {
-                                WebElement element2 = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/block-component/div/div[1]/div/div/div/div/div[1]/div/div/div/div/div[1]/div/div[2]/table/tbody/tr[2]/td[3]/b"));
-                                WebElement element3 = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/block-component/div/div[1]/div/div/div/div/div[1]/div/div/div/div/div[1]/div/div[2]/table/tbody/tr[2]/td[2]"));
-                                if (element3.getText().equalsIgnoreCase(city)) {
-
-                                    pincode = element2.getText();
-                                    System.out.println(pincode);
-
-                                }
-                            } catch (NoSuchElementException e2) {
-                                System.out.println("All elements not found at Row Number: " + rowNum + ", setting pincode as 'Not Found'.");
-                                pincode = "Not Found";
-                            }
+                        WebElement element2 = driver.findElement(By.xpath("//*[@id=\"b_results\"]/li[1]/h2/a"));
+                        WebElement element3 = driver.findElement(By.xpath("//span[@style='font-size:250%; font-weight:bold;']"));
+                        if (element2.isDisplayed()) {
+                            JavascriptExecutor js = (JavascriptExecutor) driver;
+                            js.executeScript("arguments[0].removeAttribute('target');", element2);
+                            element2.click();
+                            Thread.sleep(2000);
+                            pincode=element3.getText();
+                            System.out.println(pincode);
+                            driver.navigate().back();
                         }
+                    } catch (NoSuchElementException e2) {
+                        System.out.println("All elements not found at Row Number: " + rowNum + ", setting pincode as 'Not Found'.");
+                        pincode = "Not Found";
                     }
                 }
             }
@@ -128,7 +87,7 @@ public  class pincode {
             pincodeCell.setCellValue(pincode);
         }
 
-        FileOutputStream outputStream = new FileOutputStream("C:\\DQG\\DQG_WEB\\src\\main\\java\\Resources\\Book1k.xlsx");
+        FileOutputStream outputStream = new FileOutputStream("C:\\DQG\\DQG_WEB\\src\\main\\java\\Resources\\Book7500.xlsx");
         workbook.write(outputStream);
         workbook.close();
         outputStream.close();
